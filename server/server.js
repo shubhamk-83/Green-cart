093508ad90c55f1ca674f785e5915b4dd35f7723
shubhamk-83 <shubhamrajput5641@gmail.com>
2026-06-03 00:11:31 +0530

    fixed server

--- server/server.js
+++ server/server.js
@@ -30,7 +30,13 @@ const allowedOrigins = [
 // CORS Middleware
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
